de4e2c9ac1aa2321954c4728c9dc32693cc92366
MariselaSA <mariselaestudy@gmail.com>
2024-08-19 12:23:46 -0500

    subiendo hito 2 y parte de hito 3

--- src/view.js
+++ src/view.js
@@ -1,6 +1,15 @@
 export const renderItems = (data) => {
+  let resultado = " "
+
   console.log(data)
   // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return 'example';
+  for (let sitio of data) {
+    const image=  "<img src= '"+sitio.imageUrl+"'>" ;
+    resultado = resultado + "<li>" +image+ sitio.name + "<p>"+sitio.shortDescription+"</p>"+ "</li>";
+
+
+  }
+  return resultado;
+
 };
 
